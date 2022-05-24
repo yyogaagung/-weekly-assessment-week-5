72348612956bb749242e92239286f61246b16bc7
yyogaagung <gcyogaagung123@gmail.com>
2022-05-24 19:51:14 +0700

    mengubah logic total harga menggunkan useSelector

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,10 +3,13 @@ import { useSelector } from "react-redux";
 
 export default function Footer(){
     const counter = useSelector(state => state.total);
+    const {product, price} = useSelector(state => state.total);
+    let totalHarga = 0;
+    product.map((el) => {totalHarga += el.price })
     return(
         <Fragment>
             <div className="checkout--wrap">
-                <h1 className="checkout--text">Total Belanja : {counter.price} </h1>
+                <h1 className="checkout--text">Total Belanja : {totalHarga}  </h1>
                 {counter === 0 ? '' : <a><button>checkout</button></a>}
             </div>
         </Fragment>
